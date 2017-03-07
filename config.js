0ad47fac9dbf6cc6bfffe8bce753e3a57e05b089
李奇涵 <liqihan@benditoutiao.com>
2017-03-07 20:59:02 +0800

    数据库迁到了mysql这

--- config.js
+++ config.js
@@ -56,11 +56,19 @@ config = {
         // #### Database
         // Ghost supports sqlite3 (default), MySQL & PostgreSQL
         database: {
-            client: 'sqlite3',
+            // client: 'sqlite3',
+            // connection: {
+            //     filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            // },
+            // debug: false
+            client: 'mysql',
             connection: {
-                filename: path.join(__dirname, '/content/data/ghost-dev.db')
-            },
-            debug: false
+                host: '127.0.0.1',
+                user: 'root',
+                password: '',
+                database: 'ghost',
+                charset: 'utf8'
+            }
         },
         // #### Server
         // Can be host & port (default), or socket
